f63f449ddac07e2ceeb3c4b9b9918288275106dd
MedHedi19 <bensalahm99@gmail.com>
2026-08-16 01:24:09 +0100

    a

--- src/firewall/threat-feed.scheduler.ts
+++ src/firewall/threat-feed.scheduler.ts
@@ -62,7 +62,7 @@ export class ThreatFeedScheduler {
             BlockSource.AUTO,
             null,
             null,
-            { abuseScore: 95, abuseCategories: null },
+            { abuseScore: 95, abuseCategories: undefined },
           );
 
           this.logger.log(`Added IP to blacklist from threat feed: ${ip}`);
